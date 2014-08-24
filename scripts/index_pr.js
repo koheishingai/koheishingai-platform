71c9e4c07fabba4beedc66330c3afb7249252f54
koheishingai <koheishingai@gmail.com>
2014-08-24 16:41:51 +0900

    add something

---
+++ scripts/index_pr.js
@@ -0,0 +1,133 @@
+/*
+Author: Kohei Shingai
+Requires: jquery
+*/
+(function() {
+    "use strict"
+    var _width, _height, _device, _browser, _url, _timer, _timer_l, _cnt_l, $body, $logo, $card, $loading, $title_l, $search_w, $side_nav, init, logoPos, upAd, nLoad, iLoad, alertS, read;
+    var socket_r = io.connect(":8080");
+    var socket_w = io.connect(":8081");
+    var socket_b = io.connect(":8082");
+    _width = window.innerWidth;
+    _height = window.innerHeight;
+    _device = localStorage.getItem("device");
+    _browser = localStorage.getItem("browser");
+    _url = window.location.pathname;
+    $logo = $('.logo');
+    $card = $('.card');
+    $loading = $('.loading');
+    $title_l = $('.title_l');
+    $search_w = $('.search_w');
+    $side_nav = $('.side-nav');
+    $body = $('body');
+    read = function(){
+      socket_r.emit("read", _url);
+    }
+    alertS = function(){
+      if(_device === "iphone" && _browser === "safari"){
+        var height = (0 - _height) + 80;
+        setTimeout(function(){
+          $body.css("background", "#eee");
+          $side_nav.css("margin-top", height);
+        }, 90);
+      }
+    };
+    upAd = function(){
+      setTimeout(function(){
+        $card.addClass("up");
+      }, 420);
+    };
+    logoPos = function(w, h){
+      var h2 = (h - 184)/2;
+      if(_device !== "iphone"){
+        if (w > 580) {
+          $logo.css("margin-top", "20px");
+        }else{
+          $logo.css("margin-top", h2-376);
+        }
+      }else{
+        if (w > h) {
+          $logo.css("margin-top", h2-154);
+          $search_w.hide();
+        }else{
+          $logo.css("margin-top", h2-256);
+          $search_w.show();
+        }
+      }
+    };
+    nLoad = function(title_l){
+      if(_cnt_l === 0){
+        $title_l.text(title_l).removeClass("step4").addClass("step1");
+        read();
+      }else if(_cnt_l === 1){
+        $title_l.removeClass("step1").addClass("step2");        
+      }else if(_cnt_l === 2){
+        $title_l.removeClass("step2").addClass("step3");
+      }else if(_cnt_l === 3){
+        $title_l.removeClass("step3").addClass("step4");
+        if(_url !== "/"){
+          $loading.fadeOut(function(){
+            clearInterval(_timer_l);
+            logoPos(_width, _height);
+            upAd();
+          });
+        }else{
+          $loading.fadeOut(function(){
+            clearInterval(_timer_l);
+            logoPos(_width, _height);
+            upAd();
+          });
+        }
+      }
+    };
+    iLoad = function(title_l){
+      if(_cnt_l === 0){
+        $title_l.html(title_l);
+        read();
+      }else if(_cnt_l === 1){
+               
+      }else if(_cnt_l === 2){
+        
+      }else if(_cnt_l === 3){
+        
+        if(_url !== "/"){
+          _cnt_l = -1;
+        }else{
+          logoPos(_width, _height);
+          $loading.fadeOut(function(){
+            clearInterval(_timer_l);
+            upAd();
+            alertS();
+          });
+        }
+      }
+    };
+    init = function(){
+      _cnt_l = 0;
+      _timer_l = setInterval(function(){
+        if(_url === "/"){
+          var title_l = "Kohei Shingai";
+        }else{
+          var title_l = _url.split("/").join("");
+              title_l = title_l.split("_").join(" ");
+        }
+        if(_device !== "iphone"){
+          nLoad(title_l);
+        }else{
+          iLoad(title_l+"<div>Now Loading</div>");
+        }
+        _cnt_l++;
+      }, 730);
+    };
+    init();
+    $(window).resize(function() {
+        var width = window.innerWidth;
+        var height = window.innerHeight;
+        if (_timer !== false) {
+            clearTimeout(_timer)
+        }
+        _timer = setTimeout(function() {
+            logoPos(width, height);
+        }, 100)
+    });
+})();
---
+++ scripts/lang_pr.js
@@ -0,0 +1,94 @@
+/*
+Author: Kohei Shingai
+Requires: jquery
+*/
+(function(){
+  "use strict"
+  var init, getLang, setLang, getTime, getJson, setContent, viewNotify, _lang, _json, $on, $off, $html, $head, $notify, $search_b;
+  var socket=io.connect(":8082");
+  $on = $("#switch-on");
+  $off = $("#switch-off");
+  $html = $("html");
+  $head = $('head');
+  $notify = $('.notify');
+  $search_b = $('.search_b');
+  viewNotify = function(lang){
+    if(lang === "ja"){
+      var alert   = "日本語に設定しました"
+      var alert_s = "Kohei Shingaiを検索"
+    }else{
+      var alert = "Set to English"
+      var alert_s = "Search in Kohei Shingai"
+    }
+    $notify.text(alert).addClass("on");
+    $search_b.attr("placeholder", alert_s);
+    setTimeout(function(){
+      $notify.removeClass("on");
+    }, 1200);
+  };
+  setContent = function(){
+    for(var key in _json){
+      if(key.indexOf("_"+_lang) !== -1){
+        var w = key.split("_")[0];
+        var c = _json[key];
+        $("[data='"+w+"']").html(c);
+      }
+    }
+  };
+  getJson = function(){
+    _json = {"about_en":"About","about_ja":"自己紹介","product_en":"Product","product_ja":"プロダクト","article_en":"Article","article_ja":"記事","ad1_en":"Create your portfolio.","ad1_ja":"ポートフォリオ<br>をつくる","logsig_en":"Login / Sign Up","logsig_ja":"ログイン / 登録","rel_en":"Related Pages","rel_ja":"関連ページ","menu_en":"Menu","menu_ja":"メニュー"};
+  };
+
+  getTime = function(){
+    var t = new Date();
+    var h = t.getHours();
+    var m = t.getMinutes();
+    var s = t.getSeconds();
+    return "" + h + "" + m + "" + s;
+  };
+  getLang = function(){
+    try{
+      return (navigator.browserLanguage || navigator.language || navigator.userLanguage).substr(0,2);
+    }catch(e){
+      return undefined;
+    }
+  };
+  setLang = function(){
+    _lang = getLang();
+    $head.append('<link class="langstyle" rel="stylesheet" href="../styles/main_'+_lang+'.css?'+getTime()+'" type="text/css">');
+    if(_lang === 'ja'){
+      $html.attr("lang", "ja");
+      $on.removeAttr("checked");
+      $off.attr("checked", "");
+      setContent();
+      $search_b.attr("placeholder", "Kohei Shingaiを検索");
+    }else{
+      $html.attr("lang", "en");
+      $on.attr("checked", "");
+      $off.removeAttr("checked");
+      setContent();
+      $search_b.attr("placeholder", "Search in Kohei Shingai");
+    }
+  };
+  init = function(){
+    getJson();
+    setLang();
+  };
+  init();
+  $on.change(function(){// To English
+    _lang = "en";
+    $('.langstyle').remove();
+    $head.append('<link class="langstyle" rel="stylesheet" href="../styles/main_en.css?'+getTime()+'" type="text/css">');
+    $html.attr("lang", "en");
+    setContent();
+    viewNotify(_lang);
+  });
+  $off.change(function(){// To Japanese
+    _lang = "ja";
+    $('.langstyle').remove();
+    $head.append('<link class="langstyle" rel="stylesheet" href="../styles/main_ja.css?'+getTime()+'" type="text/css">');
+    $html.attr("lang", "en");
+    setContent();
+    viewNotify(_lang);
+  });
+})();
